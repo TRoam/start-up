a01164c2431a55363c8e6acb187b54d37e2fdc4b
Roam Tang <roam.tang@sap.com>
2016-07-05 13:56:29 +0800

    init commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import styles from './styles.module.css'
+import 'font-awesome/css/font-awesome.css'
+import './app.css'
+
+const App = React.createClass({
+   render: function(){
+     return (<div className={styles.wrapper}>
+        <h1>
+        <i className="fa fa-star"></i>
+        ENV: {__NODE_ENV__}
+        </h1>
+       </div>
+     );
+   }
+});
+
+const mountNode = document.querySelector('#root');
+ReactDOM.render(<App />,mountNode);
